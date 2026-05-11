57d49cda11fe6e773fef03f5df59195e27b938f3
David Berkompas <david.berkompas@gmail.com>
2026-05-11 14:16:11 -0500

    Adding core heroes

--- src/data/playerCards/index.ts
+++ src/data/playerCards/index.ts
@@ -1,10 +1,13 @@
 import { spiderManPlayerCards } from "./spiderMan";
+import { captainMarvelPlayerCards } from "./captainMarvel";
+import { blackPantherPlayerCards } from "./blackPanther";
+import { sheHulkPlayerCards } from "./sheHulk";
+import { ironManPlayerCards } from "./ironMan";
 import { basicPlayerCards } from "./basic";
 import { aggressionPlayerCards } from "./aggression";
 import { justicePlayerCards } from "./justice";
 import { leadershipPlayerCards } from "./leadership";
 import { protectionPlayerCards } from "./protection";
-import { captainMarvelPlayerCards } from "./captainMarvel";
 
 export const allPlayerCards = [
     ...spiderManPlayerCards,
@@ -14,6 +17,9 @@ export const allPlayerCards = [
     ...leadershipPlayerCards,
     ...protectionPlayerCards,
     ...captainMarvelPlayerCards,
+    ...blackPantherPlayerCards,
+    ...sheHulkPlayerCards,
+    ...ironManPlayerCards,
 ];
 
 export const playerCardByCode = new Map(

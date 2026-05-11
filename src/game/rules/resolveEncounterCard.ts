3e8e4f38974461226ea8fa43ab7a26b5064e3cc5
David Berkompas <david.berkompas@gmail.com>
2026-05-11 18:27:23 -0500

    Updating resolveEncounters

--- src/game/rules/resolveEncounterCard.ts
+++ src/game/rules/resolveEncounterCard.ts
@@ -6,20 +6,54 @@ export function resolveEncounterCardEffect(
 ): Partial<GameState> {
     if (card.type === "minion") {
         return {
-            minions: [...state.minions, card],
-            log: [...state.log, `${card.name} engaged as a minion.`],
+            minions: [
+                ...state.minions,
+                {
+                    ...card,
+                    currentHitPoints: card.hp ?? 0,
+                },
+            ],
+            log: [
+                ...state.log,
+                `${card.name} engaged as a minion.`,
+            ],
         };
     }
 
     if (card.type === "sideScheme") {
         return {
-            sideSchemes: [...state.sideSchemes, card],
-            log: [...state.log, `${card.name} entered play as a side scheme.`],
+            sideSchemes: [
+                ...state.sideSchemes,
+                card,
+            ],
+            log: [
+                ...state.log,
+                `${card.name} entered play as a side scheme.`,
+            ],
+        };
+    }
+
+    if (card.type === "treachery") {
+        return {
+            encounterDiscard: [
+                ...state.encounterDiscard,
+                card,
+            ],
+            log: [
+                ...state.log,
+                `Resolved treachery: ${card.name}.`,
+            ],
         };
     }
 
     return {
-        encounterDiscard: [...state.encounterDiscard, card],
-        log: [...state.log, `${card.name} resolved and discarded.`],
+        encounterDiscard: [
+            ...state.encounterDiscard,
+            card,
+        ],
+        log: [
+            ...state.log,
+            `${card.name} resolved and discarded.`,
+        ],
     };
 }

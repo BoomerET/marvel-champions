192533b1e9477624e84af46d1d31cc20c2fb37c0
David Berkompas <david.berkompas@gmail.com>
2026-05-14 08:50:27 -0500

    Updating player hand

--- src/components/PlayerHand.tsx
+++ src/components/PlayerHand.tsx
@@ -1,7 +1,9 @@
 import { useGameStore } from "../store/gameStore";
 import { CardView } from "./CardView";
+import { useState } from "react";
 
 export function PlayerHand() {
+  const [open, setOpen] = useState(true);
   const hand = useGameStore((s) => s.hero.hand);
   const pendingPayment = useGameStore((s) => s.hero.pendingPayment);
   const beginPlayCard = useGameStore((s) => s.beginPlayCard);
@@ -9,34 +11,43 @@ export function PlayerHand() {
 
   return (
     <section>
-      <h2>Player Hand ({hand.length})</h2>
+      <h2>
+        Player Hand ({hand.length}){" "}
+        <button onClick={() => setOpen((current) => !current)}>
+          {open ? "Hide" : "Show"}
+        </button>
+      </h2>
 
-      <div className="player-hand">
-        {hand.map((card) => {
-          const isPaymentCard =
-            pendingPayment?.paidWith.some(
-              (c) => c.instanceId === card.instanceId
-            ) ?? false;
 
-          const isCardBeingPlayed =
-            pendingPayment?.cardToPlay.instanceId === card.instanceId;
 
-          return (
-            <CardView
-              key={card.instanceId}
-              card={card}
-              size="small"
-              isSelected={isPaymentCard}
-              isPendingPlay={isCardBeingPlayed}
-              onClick={() =>
-                pendingPayment
-                  ? togglePaymentCard(card.instanceId)
-                  : beginPlayCard(card.instanceId)
-              }
-            />
-          );
-        })}
-      </div>
+      {open && (
+        <div className="player-hand">
+          {hand.map((card) => {
+            const isPaymentCard =
+              pendingPayment?.paidWith.some(
+                (c) => c.instanceId === card.instanceId
+              ) ?? false;
+
+            const isCardBeingPlayed =
+              pendingPayment?.cardToPlay.instanceId === card.instanceId;
+
+            return (
+              <CardView
+                key={card.instanceId}
+                card={card}
+                size="small"
+                isSelected={isPaymentCard}
+                isPendingPlay={isCardBeingPlayed}
+                onClick={() =>
+                  pendingPayment
+                    ? togglePaymentCard(card.instanceId)
+                    : beginPlayCard(card.instanceId)
+                }
+              />
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 }
